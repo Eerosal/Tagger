1d93e61e96067d15f54bbc1e3683fc2e00a98e42
Eerosal <28145295+Eerosal@users.noreply.github.com>
2022-01-30 18:47:53 +0200

    init spring project

---
+++ src/test/java/fi/eerosalla/web/tagger/TaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.eerosalla.web.tagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
