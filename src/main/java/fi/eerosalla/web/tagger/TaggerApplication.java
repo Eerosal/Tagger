d2dd050d207642e472159a2439eac4f47e314f1c
Eerosal <28145295+Eerosal@users.noreply.github.com>
2022-01-30 19:02:18 +0200

    add checkstyle

--- src/main/java/fi/eerosalla/web/tagger/TaggerApplication.java
+++ src/main/java/fi/eerosalla/web/tagger/TaggerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaggerApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(TaggerApplication.class, args);
     }
 
