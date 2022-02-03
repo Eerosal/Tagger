b5a174b12d99801044064a0b7ed243d3b8b3f377
Eerosal <28145295+Eerosal@users.noreply.github.com>
2022-02-03 14:27:06 +0200

    fix table indexes

--- src/main/java/fi/eerosalla/web/tagger/repository/tag/TagEntry.java
+++ src/main/java/fi/eerosalla/web/tagger/repository/tag/TagEntry.java
@@ -1,31 +1,26 @@
 package fi.eerosalla.web.tagger.repository.tag;
 
+import com.j256.ormlite.field.DatabaseField;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Index;
 import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "tagseq")
-@Table(name = "tg_tags", indexes = {
-        @Index(name = "idx_tags_name", columnList = "name", unique = true)
-})
 public class TagEntry {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idgen")
     private Integer id;
 
-    @Column(length = 32)
+    @DatabaseField(uniqueIndexName = "idx_tags_name")
     private String name;
 
 }
